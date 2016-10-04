3870a5ab82fcc0bfc1eb76dd615340232d7553b8
benaryorg <binary@benary.org>
2016-10-04 14:19:30 +0200

    named fields
    Signed-off-by: benaryorg <binary@benary.org>

--- src/Main.hs
+++ src/Main.hs
@@ -5,10 +5,11 @@ import Data.List
 import Data.Map (insertWith,empty,toAscList)
 
 -- "tweet_id","in_reply_to_status_id","in_reply_to_user_id","timestamp","source","text","retweeted_status_id","retweeted_status_user_id","retweeted_status_timestamp","expanded_urls"
-data Tweet = Tweet String
-
-instance Show Tweet where
-	show (Tweet text) = text
+data Tweet = Tweet
+	{
+		tweet_id :: String,
+		text :: String
+	} deriving (Show)
 
 parseHeaders :: Parser ()
 parseHeaders = do
@@ -27,7 +28,8 @@ parseCSVLine :: Parser Tweet
 parseCSVLine = do
 	fields <- parseQuoted `sepBy1` char ','
 	-- only return text for now
-	return $ Tweet $ fields!!5
+	--return $ Tweet (fields!!5)
+	return $ Tweet (fields!!0) (fields!!5)
 
 parseArchive :: Parser [Tweet]
 parseArchive = do
@@ -45,11 +47,11 @@ charCount = toAscList . foldl (\map e -> insertWith (+) e 1 map) empty
 stat :: [Tweet] -> IO ()
 stat tweets = do
 	putStr "mean character count: "
-	putStrLn $ show $ mean $ map (\(Tweet t) -> length t) tweets
+	putStrLn $ show $ mean $ map (length . text) tweets
 	putStr "number of characters: "
-	putStrLn $ show $ length $ concatMap (\(Tweet t) ->  t) tweets
+	putStrLn $ show $ length $ concatMap text tweets
 	putStr "most used characters: "
-	putStrLn $ show $ take 5 $ map (\(e,_) -> e) $ sortBy (\(_,n1) (_,n2) -> n2 `compare` n1) $ charCount $ concatMap (\(Tweet t) ->  t) tweets
+	putStrLn $ show $ take 5 $ map (\(e,_) -> e) $ sortBy (\(_,n1) (_,n2) -> n2 `compare` n1) $ charCount $ concatMap text tweets
 
 main :: IO ()
 main = do
